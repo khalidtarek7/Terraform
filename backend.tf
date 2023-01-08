24f1f3c629ab4142d71477333fabe4cdacc53981
khalid <khalidtico19@gmail.com>
2023-01-08 16:52:08 +0200

    first commit

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "khalid-s3"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "iti"
+  }
+}
---
+++ igw.tf
@@ -0,0 +1,7 @@
+resource "aws_internet_gateway" "mygateway" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
